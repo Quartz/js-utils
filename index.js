82c04486931dbf16a4bcd4d12fb01edcf74b942d
James Shakespeare <j@jshakespeare.com>
2020-10-13 17:44:37 -0400

    export arrayFromRange from index

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+export { default as arrayFromRange } from './arrayFromRange';
 export { default as minifyCss } from './minifyCss';
 export { default as hexToRgb } from './hexToRgb';
 export { default as resizeImage } from './resizeImage';
