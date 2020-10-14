744902057ef363f55272fd6784a4bd73ca905085
James Shakespeare <j@jshakespeare.com>
2020-10-14 11:35:10 -0400

    use non es6 syntax for exporting modules from index

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-export { default as arrayFromRange } from './arrayFromRange';
-export { default as minifyCss } from './minifyCss';
-export { default as hexToRgb } from './hexToRgb';
-export { default as resizeImage } from './resizeImage';
+const arrayFromRange = require( './arrayFromRange' );
+const minifyCss = require( './minifyCss' );
+const hexToRgb = require( './hexToRgb' );
+const resizeImage = require( './resizeImage' );
+
+module.exports = {
+	arrayFromRange,
+	minifyCss,
+	hexToRgb,
+	resizeImage,
+};
