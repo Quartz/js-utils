91f26890870faf11239719a8aebcba3834c27d09
James Shakespeare <j@jshakespeare.com>
2020-10-20 17:26:30 -0400

    remove time functions, do it in a separate branch

--- index.ts
+++ index.ts
@@ -1,7 +1,4 @@
 module.exports.arrayFromRange = require( './arrayFromRange' );
 module.exports.hexToRGB = require( './hexToRgb' );
 module.exports.minifyCss = require( './minifyCss' );
-module.exports.parseDateGMT = require( './parseDateGMT' );
 module.exports.resizeWPImage = require( './resizeWPImage' );
-module.exports.stylizedTimestamp = require( './stylizedTimestamp' );
-module.exports.timeAgo = require( './timeAgo' );
