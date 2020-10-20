da9b66bb7d4e27b7b700c8b986eda8113723130d
James Shakespeare <j@jshakespeare.com>
2020-10-20 12:11:33 -0400

    add parseDateGMT with test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports.arrayFromRange = require( './arrayFromRange' );
 module.exports.minifyCss = require( './minifyCss' );
+module.exports.parseDateGMT = require( './parseDateGMT' );
 module.exports.hexToRGB = require( './hexToRgb' );
 module.exports.resizeWPImage = require( './resizeWPImage' );
---
+++ parseDateGMT.test.js
@@ -0,0 +1,29 @@
+const { parseDateGMT } = require( '.' );
+
+describe( 'Dates: parseDateGMT', () => {
+	it( 'can parse lots of date formats correctly', () => {
+		const dates = [
+			'2018-01-01 12:00:00.000Z',
+			'2018-01-01 12:00:00',
+			'2018/01/01 12:00:00',
+			'2018-01-01T12:00:00',
+			'2018/01/01T12:00:00',
+			'2018-01-01T12:00',
+			'2018-01-01 12:00',
+			'2018/01/01T12:00',
+		];
+
+		dates.forEach( date => expect( parseDateGMT( date ).toGMTString() ).toEqual( 'Mon, 01 Jan 2018 12:00:00 GMT' ) );
+	} );
+
+	it( 'handles null inputs', () => {
+		const dates = [
+			'',
+			null,
+			undefined,
+			false,
+		];
+
+		dates.forEach( date => expect( parseDateGMT( date ) instanceof Date ).toEqual( true ) );
+	} );
+} );
